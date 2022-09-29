63eebb85d3e6246406501d8e5fa6a8d43917a2c6
= <=>
2022-09-29 14:47:33 +0900

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>{{ setData }}</p>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  setup() {
+    const setData = [];
+
+    const shoppingData = async () => {
+      const URL = "https://openapi.naver.com/v1/search/movie.json";
+      const ClientID = "DeHyXTOI0F6OhYCzWQk8";
+      const ClientSecret = "0RDDq8mv2N";
+
+      await axios
+        .get(URL, {
+          headers: {
+            "X-Naver-Client-Id": ClientID,
+            "X-Naver-Client-Secret": ClientSecret,
+          },
+        })
+        .then((res) => setData(res.data))
+        .catch((err) => console.log(err));
+    };
+    shoppingData();
+
+    return {
+      setData,
+    };
+  },
+};
+</script>
+
+<style></style>
